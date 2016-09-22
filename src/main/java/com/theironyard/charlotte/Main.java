91b35cea33905e687f11fa03caa5a2065ee44629
Ben Sterrett <raynjamin@gmail.com>
2016-09-21 20:32:58 -0400

    Adding a bunch of maven stuff

--- src/main/java/com/theironyard/charlotte/Main.java
+++ src/main/java/com/theironyard/charlotte/Main.java
@@ -7,6 +7,7 @@ import static spark.Spark.*;
 public class Main {
 
     public static void main(String[] args) {
+        port(Integer.valueOf(System.getenv("PORT"));
 
         webSocket("/people-game", GameWebSocket.class);
 
